545f99755eb13e2ee94a35eaf63f12eb6f4ae627
sanket723 <sanketpawar723@gmail.com>
2023-02-11 22:57:32 +0530

    first submit vehicle assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,6 +3,7 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,4,4,6,isManual,"type",2);
         //Use arbitrary values for parameters which are not mentioned
     }
 
@@ -17,14 +18,46 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        newSpeed = getCurrentSpeed()+rate;
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        else if(newSpeed > 0 && newSpeed <= 50) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(1);
+            //changeGear(1);
         }
+
+        else if(newSpeed > 50 && newSpeed <= 100){
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(2);
+            //changeGear(2);
+        }
+        else if(newSpeed > 100 && newSpeed <= 150){
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(3);
+            //changeGear(3);
+        }
+        else if(newSpeed > 150 && newSpeed <= 200){
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(4);
+           // changeGear(4);
+        }
+        else if(newSpeed > 200 && newSpeed <= 250){
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(5);
+            //changeGear(5);
+        }
+        else if(newSpeed>250){
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(6);
+            //changeGear(6);
+        }
+
     }
 }
